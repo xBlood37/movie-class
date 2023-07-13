93c3079e0f52ad81954781458dd94df4c8065ae1
xBlood37 <123d.123b@mail.ru>
2023-07-13 14:02:01 +0300

    rate error

--- src/services/film.js
+++ src/services/film.js
@@ -10,7 +10,7 @@ const options = {
 
 export const getFilm = async (current = 1) => {
   const res = await fetch(
-    `${baseUrl}discover/movie?include_adult=false&include_video=false&language=ru-US&page=${current}&sort_by=popularity.desc`,
+    `${baseUrl}discover/movie?api_key=019f45ffad25df23ae4bc380bfe0d72f&include_adult=false&include_video=false&language=ru-US&page=${current}&sort_by=popularity.desc`,
     options,
   );
   const data = await res.json();
@@ -28,14 +28,20 @@ export const searchFilm = async (label) => {
 
 const guestSession = async () => {
   const res = await fetch(
-    `https://api.themoviedb.org/3/authentication/guest_session/new`,
+    `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=019f45ffad25df23ae4bc380bfe0d72f`,
     options,
   );
   const data = await res.json();
-  return data.guest_session_id;
+  // console.log(data.guest_session_id);
+  localStorage.setItem('guest_session_id', data.guest_session_id);
+  // return data.guest_session_id;
 };
 
-const sessionId = guestSession();
+await guestSession();
+
+// const sessionId = guestSession();
+const sessionId = localStorage.getItem('guest_session_id');
+console.log(sessionId);
 
 export const addRating = async (id, rate) => {
   const options = {
@@ -50,17 +56,22 @@ export const addRating = async (id, rate) => {
   };
 
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${id}/rating?guest_session_id=${sessionId}`,
+    `https://api.themoviedb.org/3/movie/${id}/rating?api_key=019f45ffad25df23ae4bc380bfe0d72f&guest_session_id=${sessionId}`,
     options,
   );
-  console.log(res.json());
+  const data = await res.json();
+  console.log(data);
+  // console.log(res.json());
 };
 
 export const getRateFilm = async () => {
   const res = await fetch(
-    `https://api.themoviedb.org/3/guest_session/${sessionId}/rated/movies?language=en-US&page=1&sort_by=created_at.asc`,
+    `https://api.themoviedb.org/3/guest_session/${sessionId}/rated/movies?api_key=019f45ffad25df23ae4bc380bfe0d72f&page=1`,
     options,
   );
-
-  console.log(res.json());
+  console.log(sessionId);
+  const data = await res.json();
+  console.log(data);
 };
+
+// ?api_key=019f45ffad25df23ae4bc380bfe0d72f
